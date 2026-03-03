99de1b9fa8364d1a05bc8b4e51a406c29b968b15
Madison Medeiros <117107317+madisonmedeiros@users.noreply.github.com>
2026-03-03 13:09:48 -0500

    Update cosmic_check.py

--- scripts/cosmic_check.py
+++ scripts/cosmic_check.py
@@ -1,5 +1,24 @@
-import pandas as pd
+"""
+cosmic_check.py
 
+Cross-references rare variant burden testing results against the COSMIC
+Cancer Gene Census (CGC) to identify which significant genes have a known
+role in cancer.
+
+Inputs:
+    - significant_genes.txt   : RV-Excalibur output filtered to P < 0.05 (75 genes)
+    - cancer_gene_census.csv  : COSMIC CGC downloaded from https://cancer.sanger.ac.uk/cosmic
+
+Output:
+    - significant_cancer_genes.txt : Significant genes overlapping with COSMIC CGC
+
+Usage:
+    python3 cosmic_check.py
+
+Author: Madison Medeiros
+"""
+
+import pandas as pd
 
 sig_genes = pd.read_csv("significant_genes.txt", sep=r"\s+") 
 sig_genes_list = sig_genes['Gene'].tolist() # Add genes to list
